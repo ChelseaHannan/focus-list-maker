1248bf6231e6c0ac3110f4c587749307e20c37ae
Chelsea <chelseahannan@icloud.com>
2020-05-21 14:13:34 -0400

    update code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # focus-list-maker
-make your own to-do list, add/delete items, cross off completed items
+An simple todo app, using html and javascript (jquery).
+
+Main Features:
+1. Creating new tasks.
+2. Marking a task as complete by clicking on the item. This creates a strike-through over the item.
+3. Deleting tasks.
+
+Future Functionality:
+1. Persistent list between refreshes.
+2. View deleted tasks.
+3. Un-delete tasks.
+4. Edit tasks.
+5. Re-order tasks via Drag n Drop.
+
+
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ol = document.querySelector("ol");
+let button = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let ol = document.querySelector("ol");
 
 function inputLength() {
 	return input.value.length;
@@ -8,20 +8,20 @@ function inputLength() {
 
 
 function createListElement() {
-	var li = document.createElement("li");
+	let li = document.createElement("li");
 	// li.appendChild(document.createElement("br"));
 	li.appendChild(document.createTextNode(input.value));
 
-  li.addEventListener("click", function(event) {
+  	li.addEventListener("click", function(event) {
 			event.target.classList.toggle("done");
 	});
 
 	ol.appendChild(li);
 	input.value = "";
 	li.appendChild(document.createTextNode("  "));
-	var delButton = li.appendChild(document.createElement("button"));
+	let delButton = li.appendChild(document.createElement("button"));
 	delButton.appendChild(document.createTextNode("Delete"));
-  delButton.className = "btn btn-small btn-info";
+  	delButton.className = "btn btn-small btn-info";
 
 	delButton.addEventListener("click", function(event) {
 		event.target.parentElement.classList.add("goAway");
